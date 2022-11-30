f46c4045ba6ae9100ed4b3f32e23670074ce55c4
Dmitry <61630918+DmitryDol@users.noreply.github.com>
2022-11-30 14:18:39 +0400

    Update mp5 lab

--- mp5.cpp
+++ mp5.cpp
@@ -32,6 +32,57 @@ private:
 	int number_of_branch;
 	int amount_of_days;
 	double* income;
+
+	// является ли введенное значение числом
+	bool is_pos_int(std::string input) {
+		for (char i : input)
+			if (!std::isdigit(i))
+				return false;
+		return true;
+	}
+	bool is_double(std::string input) {
+		if (std::count(input.begin(), input.end(), '.') > 1 || std::count(input.begin(), input.end(), '-') > 1 ||
+			(std::count(input.begin(), input.end(), '-') == 1 && input[0] != '-'))
+			return false;
+		for (char i : input)
+			if (!std::isdigit(i) && i != '-' && i!='.')
+				return false;
+		return true;
+	}
+
+	// для ввода значений из файла. если в файле найдено некорректное значение throw runtime_error
+	int int_input(std::ifstream& in) {
+		std::string input;
+		in >> input;
+		if (!is_pos_int(input))
+			throw std::runtime_error("Invalid value in file. Please check input file");
+		return std::stoi(input);
+	}
+	// запрашиваем значения, пока не ввели корректные
+	int int_input(std::istream& in, std::string message) {
+		std::string input;
+		do {
+
+			std::cout << message;
+			std::getline(in, input);
+		} while (!is_pos_int(input));
+		return std::stoi(input);
+	}
+	double double_input(std::istream& in) {
+		std::string input;
+		do {
+			in >> input;
+		} while (!is_double(input));
+		return std::stod(input);
+	}
+	double double_input(std::ifstream& in) {
+		std::string input;
+		in >> input;
+		if (!is_double(input))
+			throw std::runtime_error("Invalid value in file. Please check input file.");
+		return std::stod(input);
+	}
+	
 public:
 	Income_data() {
 		number_of_branch = 0;
@@ -64,7 +115,8 @@ public:
 		amount_of_days = 0;
 		number_of_branch = 0;
 	}
-	
+
+	// вычисляяем количество дней с доходом равным 0
 	int amount_of_zero_income_days() const {
 		int count = 0;
 		for (int i = 0; i < amount_of_days; ++i)
@@ -73,48 +125,52 @@ public:
 		return count;
 	}
 	
-	
-	friend std::ifstream& operator >> (std::ifstream& in, Income_data& ref);
-	friend std::istream& operator >> (std::istream& in, Income_data& ref);
-	
-	friend std::ostream& operator << (std::ostream& out, const Income_data& ref);
-};
 
-std::ifstream& operator >> (std::ifstream& in, Income_data& ref) {
-	in >> ref.number_of_branch >> ref.amount_of_days;
-	if (ref.income != nullptr)
-		delete[] ref.income;
-	ref.income = new double[ref.amount_of_days];
-	for (int i = 0; i < ref.amount_of_days; ++i)
-		in >> ref.income[i];
-	return in;
-}
-std::istream& operator >> (std::istream& in, Income_data& ref) {
-	std::cout << "Enter number of branch: ";
-	in >> ref.number_of_branch;
-	std::cout << "Enter amount of days: ";
-	in >> ref.amount_of_days;
-	if (ref.income != nullptr)
-		delete[] ref.income;
-	std::cout << "Enter income for each of " << ref.amount_of_days << " days: ";
-	
-	ref.income = new double[ref.amount_of_days];
-	for (int i = 0; i < ref.amount_of_days; ++i) {
-		in >> ref.income[i];
+	friend std::ifstream& operator >> (std::ifstream& in, Income_data& ref) {
+		try {
+			ref.number_of_branch = ref.int_input(in);
+			ref.amount_of_days = ref.int_input(in);
+			if (ref.income != nullptr)
+				delete[] ref.income;
+			ref.income = new double[ref.amount_of_days];
+			for (int i = 0; i < ref.amount_of_days; ++i)
+				ref.income[i] = ref.double_input(in);
+			return in;
+		}
+		catch (std::runtime_error& e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
-	if (in.peek() == '\n')
-		in.get();
-	return in;
-}
+	friend std::istream& operator >> (std::istream& in, Income_data& ref){
+		ref.number_of_branch = ref.int_input(in, "Enter number of branch: ");
 
-std::ostream& operator << (std::ostream& out, const Income_data& ref) {
-	for (int i = 0; i < ref.amount_of_days; ++i)
-		out << ref.income[i] << ' ';
-	out << std::endl << "Amount of days when income of branch number "
-		<< ref.number_of_branch << " was equal to 0: "
-		<< ref.amount_of_zero_income_days();
-	return out;
-}
+		ref.amount_of_days = ref.int_input(in, "Enter amount of days: ");
+
+		if (ref.income != nullptr)
+			delete[] ref.income;
+
+		std::cout << "Enter income for each of " << ref.amount_of_days << " days: ";
+		ref.income = new double[ref.amount_of_days];
+		for (int i = 0; i < ref.amount_of_days; ++i) {
+			ref.income[i] = ref.double_input(in);
+		}
+		if (in.peek() == '\n')
+			in.get();
+		return in;
+	}
+	
+	friend std::ostream& operator << (std::ostream& out, const Income_data& ref) {
+		for (int i = 0; i < ref.amount_of_days; ++i)
+			out << ref.income[i] << ' ';
+		out << std::endl << "Amount of days when income of branch number "
+			<< ref.number_of_branch << " was equal to 0: "
+			<< ref.amount_of_zero_income_days();
+		return out;
+	}
+	friend bool operator != (Income_data& a, Income_data& b) {
+		return a.amount_of_days!=b.amount_of_days;
+	}
+};
 
 void file_input(Income_data& data) {
 	std::cout << "Input path.\n";
@@ -169,7 +225,14 @@ void output(Income_data& data) {
 int main()
 {
 	Income_data data;
-	input(data);
-	output(data);
-
+	std::string choice;
+	do {
+		input(data);
+		output(data);
+		std::cout << "\n\nPrint:\n'0' to close the program. "
+				  << "\n'1' to clear cmd and restart\nany key to restart without clearing : ";
+		std::getline(std::cin, choice);
+		if (choice == "1")
+			system("cls");
+	} while (choice != "0");
 }
